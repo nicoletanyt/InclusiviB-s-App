a0c8af6dce884d52694390ac1fb8bc4669ad67e2
Ryan Tung <88029861+Ryan-Tung@users.noreply.github.com>
2022-10-26 12:58:45 +0800

    something

--- innofest app/ContentView.swift
+++ innofest app/ContentView.swift
@@ -1,44 +1,21 @@
 //
 //  ContentView.swift
-//  Emoji Party
+//  innofest app
 //
-//  Created by Jia Chen Yee on 19/7/22.
+//  Created by RYAN TUNG TZE-JIN on 26/10/22.
 //
 
 import SwiftUI
 
 struct ContentView: View {
-    
-    var emojiSets = [
-        EmojiSet(name: "Fruits", emojis: ["🍌", "🍎", "🍓", "🍉", "🍍", "🥑"]),
-        EmojiSet(name: "Candy", emojis: ["🍫", "🍭", "🍬"]),
-        EmojiSet(name: "Chaotic Swift Students", emojis: ["😾", "😬", "🤡", "📢", "😵‍💫", "🐧"])
-    ]
-        
     var body: some View {
-        NavigationView {
-            List(emojiSets) { emojiSet in
-                NavigationLink {
-                    EmojiView(emojiSet: emojiSet)
-                } label: {
-                    VStack(alignment: .leading) {
-                        Text(emojiSet.name)
-                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                            
-                        Text(emojiSet.emojis.joined())
-                    }
-                }
-
-            }
-            .navigationTitle("Emoji Set")
-        }
-        .navigationViewStyle(.automatic)
+        Text("Hello, world!")
+            .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .previewInterfaceOrientation(.landscapeLeft)
     }
 }
